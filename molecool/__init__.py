5debabc55f9e38606dda07729bf70ac718a38579
Xinhong Liu <xliu27@nd.edu>
2023-06-13 21:54:55 -0600

    Give some structure to the package

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -4,5 +4,14 @@ This was created for the Python Best Practices Workshop"""
 # Add imports here
 from .functions import canvas
 
+from molecool.measure import calculate_angle
+from molecool.measure import calculate_distance
+
+from molecool.atom_data import atom_colors, atomic_weights
+from molecool.visualization import draw_molecule
+
+from molecool.molecules import bond_histogram, build_bond_list
+
+from molecool.io import open_pdb
 
 from ._version import __version__
